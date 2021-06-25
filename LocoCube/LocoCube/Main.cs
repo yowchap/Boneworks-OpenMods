eb2a87b52d63cea18cd89af375ba4c4386e77790
yowchap <50385582+yowchap@users.noreply.github.com>
2021-06-25 00:21:16 -0700

    Added LocoCube

---
+++ LocoCube/LocoCube/Main.cs
@@ -0,0 +1,27 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace YOWC.LocoCube
+{
+    public static class BuildInfo
+    {
+        public const string Name = "LocoCube"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "YOWChap"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class Main : MelonMod
+    {
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            GameObject feet = GameObject.Find("[RigManager (Default Brett)]").transform.Find("[PhysicsRig]/Feet").gameObject;
+            SphereCollider locoSphere = feet.GetComponent<SphereCollider>();
+            BoxCollider locoCube = feet.AddComponent<BoxCollider>();
+            locoCube.size = Vector3.one * locoSphere.radius * 2f;
+            locoCube.material = locoSphere.material;
+            locoSphere.enabled = false;
+        }
+    }
+}
---
+++ LocoCube/LocoCube/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(YOWC.LocoCube.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(YOWC.LocoCube.BuildInfo.Company)]
+[assembly: AssemblyProduct(YOWC.LocoCube.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + YOWC.LocoCube.BuildInfo.Author)]
+[assembly: AssemblyTrademark(YOWC.LocoCube.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(YOWC.LocoCube.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(YOWC.LocoCube.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(YOWC.LocoCube.Main), YOWC.LocoCube.BuildInfo.Name, YOWC.LocoCube.BuildInfo.Version, YOWC.LocoCube.BuildInfo.Author, YOWC.LocoCube.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("Stress Level Zero", "BONEWORKS")]

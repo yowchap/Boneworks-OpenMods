b85626de3c9668ced5fa85883405aa18af1a0c7b
yowchap <50385582+yowchap@users.noreply.github.com>
2021-06-25 00:22:01 -0700

    Added NotGoldenMags

---
+++ NotGoldenMags/NotGoldenMags/NotGoldenMagsMod.cs
@@ -0,0 +1,52 @@
+﻿using MelonLoader;
+using ModThatIsNotMod;
+using StressLevelZero.Interaction;
+using StressLevelZero.Props.Weapons;
+using UnityEngine;
+
+namespace YOWC.NotGoldenMags
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Not Golden Mags"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "YOWChap"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.1"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class NotGoldenMagsMod : MelonMod
+    {
+        private float timeBetweenMagChecks = 0.1f;
+        private float timeToCheckMags;
+
+
+        // Disclaimer: I made this before I really knew about harmony patching
+        // Resetting the mag whenever you grab one would be MUCH better than this
+        // I didn't feel like changing it though ¯\_(ツ)_/¯
+        public override void OnUpdate()
+        {
+            if (Time.time >= timeToCheckMags)
+            {
+                ResetMag(Player.leftHand);
+                ResetMag(Player.rightHand);
+
+                timeToCheckMags = Time.time + timeBetweenMagChecks;
+            }
+        }
+
+        private void ResetMag(Hand hand)
+        {
+            GameObject magGO = Player.GetObjectInHand(hand);
+            if (magGO != null)
+            {
+                Magazine mag = magGO.GetComponent<Magazine>();
+                if (mag == null)
+                    mag = magGO.GetComponentInParent<Magazine>();
+
+                if (mag != null)
+                    mag.ResetMagazine();
+            }
+        }
+    }
+}
---
+++ NotGoldenMags/NotGoldenMags/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(YOWC.NotGoldenMags.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(YOWC.NotGoldenMags.BuildInfo.Company)]
+[assembly: AssemblyProduct(YOWC.NotGoldenMags.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + YOWC.NotGoldenMags.BuildInfo.Author)]
+[assembly: AssemblyTrademark(YOWC.NotGoldenMags.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(YOWC.NotGoldenMags.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(YOWC.NotGoldenMags.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(YOWC.NotGoldenMags.NotGoldenMagsMod), YOWC.NotGoldenMags.BuildInfo.Name, YOWC.NotGoldenMags.BuildInfo.Version, YOWC.NotGoldenMags.BuildInfo.Author, YOWC.NotGoldenMags.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("Stress Level Zero", "BONEWORKS")]
